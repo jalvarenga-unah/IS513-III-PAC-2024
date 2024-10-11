9afcaf71ed67e10fe03466a98d10e1638ef5ec89
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-10-10 19:02:29 -0600

    personalizacion de widgets

--- intro-flutter/mi_primer_app/lib/main.dart
+++ intro-flutter/mi_primer_app/lib/main.dart
@@ -8,15 +8,33 @@ void main() {
 // Widgets con estado: StatefullWidget
 
 class MyApp extends StatelessWidget {
+  double size = 24.0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          // centerTitle: false,
           backgroundColor: Colors.teal,
+          title: Text(
+            'Mi primer app',
+            style: TextStyle(
+              color: Colors.teal[50],
+              fontSize: size,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
         ),
         body: Center(
           child: Text('Hola mundo'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('Hizo clic');
+          },
         ),
       ),
     );
