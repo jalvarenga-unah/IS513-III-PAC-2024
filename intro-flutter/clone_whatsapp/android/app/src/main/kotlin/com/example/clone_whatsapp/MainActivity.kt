355ff4557966734c0871ad5d3bba676943ed1a6a
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-10-21 18:52:26 -0600

    creación de proyecto de clone de whatsapp

---
+++ intro-flutter/clone_whatsapp/android/app/src/main/kotlin/com/example/clone_whatsapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package hn.edu.unah.clone_whatsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ intro-flutter/clone_whatsapp/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:clone_whatsapp/src/modules/contacts/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clone Whatsapp',
+      home: HomePage(),
+    );
+  }
+}
